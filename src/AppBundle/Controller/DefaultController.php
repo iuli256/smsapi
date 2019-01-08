fd069a3fabe6e644810502e9132b3bca68961fcc
Iulian CRACIUN <i.craciun@groupeonepoint.com>
2019-01-08 09:20:22 +0200

    add service

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Service\InputValidator;
+use AppBundle\Entity\Message;
 
 class DefaultController extends Controller
 {
@@ -17,11 +19,11 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         try{
+            $inputValidator = new InputValidator();
 
-            $Input = $this->validateInputMethod($request);
+            $Input = $inputValidator->validateInputMethod($request);
             if($Input) {
-                $this->response['status'] = 'success';
-                $this->response['message'] = 'message have been sent';
+                $this->processMessage($Input);
             }
             return $this->sendResponse($this->response);
         }
@@ -32,127 +34,26 @@ class DefaultController extends Controller
         }
     }
 
-    private function validateInputMethod($request){
-        if ($request->isMethod('POST')) {
-            return $this->validateInputHeader($request);
-        }else {
-            throw new Exception('all request should be made only using POST');
-        }
+
+
+    private function processMessage($data){
+        print_r($data);
+        $em = $this->getDoctrine()->getManager();
+
+        $newMsg = new Message();
+        $newMsg->setCreated(new \DateTime('now'));
+        $newMsg->setOriginator($data['originator']);
+        $newMsg->setRecipient($data['recipient']);
+        $newMsg->setMessage($data['message']);
+        $newMsg->setIsSent(false);
+        $em->persist($newMsg);
+        $em->flush();
+        $msg = $em->getRepository('AppBundle:Message')->findOneBy(array('isSent' => true), array('sentDate' => 'DESC'));
+        print_r($msg);
+        die();
+        $this->response['status'] = 'success';
+        $this->response['message'] = 'message have been sent';
     }
-
-    private function validateInputHeader($request){
-        if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-            return $this->validateInputContentExist($request);
-        }else{
-            throw new Exception('Content-Type should be application/json');
-        }
-    }
-
-    private function validateInputContentExist($request){
-        if ($request->getContent()) {
-            return $this->validateInputIsJson($request);
-        }else{
-            throw new Exception('no data received on POST');
-        }
-    }
-
-    private function validateInputIsJson($request){
-        try{
-            $content = $request->getContent();
-            $data = json_decode($content, true);
-            switch (json_last_error()) {
-                case JSON_ERROR_NONE:
-                    return $this->validateInputIsArray($data);
-                    break;
-                case JSON_ERROR_DEPTH:
-                    throw new Exception('Input json - Maximum stack depth exceeded');
-                    break;
-                case JSON_ERROR_STATE_MISMATCH:
-                    throw new Exception('Input json - Underflow or the modes mismatch');
-                    break;
-                case JSON_ERROR_CTRL_CHAR:
-                    throw new Exception('Input json - Unexpected control character found');
-                    break;
-                case JSON_ERROR_SYNTAX:
-                    throw new Exception('Input json - Syntax error, malformed JSON');
-                    break;
-                case JSON_ERROR_UTF8:
-                    throw new Exception('Input json - Malformed UTF-8 characters, possibly incorrectly encoded');
-                    break;
-                default:
-                    throw new Exception('Input json - Unknown error');
-                    break;
-            }
-        }catch (Exception $e){
-            throw new Exception($e->getMessage());
-        }
-    }
-
-    private function validateInputIsArray($data){
-        if (is_array($data)){
-            return $this->validateInputRecipient($data);
-        }else{
-            throw new Exception('Input json - is not an array');
-        }
-
-    }
-
-    private function validateInputRecipient($data){
-        if (isset($data['recipient'])){
-            return $this->validateRecipientFormat($data);
-        }else{
-            throw new Exception('recipient is not defined');
-        }
-
-    }
-
-    private function validateRecipientFormat($data){
-        if (preg_match('/^[1-9][0-9]{9,14}$/', $data['recipient'])) {
-            return $this->validateInputOriginator($data);
-        }else{
-            throw new Exception('recipient have not the correct format. it should be an international phone number without leading 0. ex: 40723123789');
-        }
-
-    }
-
-    private function validateInputOriginator($data){
-        if (isset($data['originator'])){
-            return $this->validateOriginatorFormat($data);
-        }else{
-            throw new Exception('originator is not defined');
-        }
-
-    }
-
-    private function validateOriginatorFormat($data){
-        if ($data['originator'] == 'MessageBird') {
-            return $this->validateInputMessage($data);
-        }else{
-            throw new Exception('originator can be only MessageBird');
-        }
-
-    }
-
-    private function validateInputMessage($data){
-
-        if (isset($data['message'])) {
-            return $this->validateMessageFormat($data);
-        }else{
-            throw new Exception('message is not defined');
-        }
-
-    }
-
-    private function validateMessageFormat($data){
-        if (preg_match('/^[a-zA-Z0-9~!@#$%^&*()`\[\]{};\':,.\/<>?| ]{1,159}$/', $data['message'])){
-            return  $data;
-        }else{
-            throw new Exception('message must not exced 160 characters');
-        }
-
-    }
-
-
     private function sendResponse($response){
 
         return new JsonResponse($response);
